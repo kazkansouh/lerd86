0f43f94029bea26f0d8b7e57b6293fdb07922713
kazza <kaz.kanso@gmail.com>
2017-04-30 17:01:58 +0100

    tweaked makefile to improve module handleing
    improved build system
    
    added missing config.mk

--- config.mk
+++ config.mk
@@ -1,9 +1,11 @@
-ROOT_DIR=/home/gfi/lerd86
+# TODO: improve handling of below variable
+ROOT_DIR=/home/kazza/lerd86
+
 SDK_ROOT=${ROOT_DIR}/externals/esp-open-sdk
 TOOLCHAIN_ROOT=${SDK_ROOT}/xtensa-lx106-elf
-SDK_INCLUDE=${SDK_ROOT}/sdk/include
-SDK_LIBS=${SDK_ROOT}/sdk/lib
-CC=${TOOLCHAIN_ROOT}/bin/xtensa-lx106-elf-gcc
-AR=${TOOLCHAIN_ROOT}/bin/xtensa-lx106-elf-ar
-STRIP=${TOOLCHAIN_ROOT}/bin/xtensa-lx106-elf-strip
-ESPTOOL=${TOOLCHAIN_ROOT}/bin/esptool.py
+SDK_INCLUDE = ${SDK_ROOT}/sdk/include
+SDK_LIBS = ${SDK_ROOT}/sdk/lib
+CC = ${TOOLCHAIN_ROOT}/bin/xtensa-lx106-elf-gcc
+AR = ${TOOLCHAIN_ROOT}/bin/xtensa-lx106-elf-ar
+STRIP = ${TOOLCHAIN_ROOT}/bin/xtensa-lx106-elf-strip
+ESPTOOL = ${TOOLCHAIN_ROOT}/bin/esptool.py
