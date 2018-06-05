fc7e3a7047919cc43ef1316aad3ad8739e2e2233
Karim Kanso <kaz.kanso@gmail.com>
2018-06-05 20:31:15 +0100

    added some string processing utility functions working with strings over multiple data packets

--- display/stringutil.c
+++ display/stringutil.c
@@ -1,6 +1,8 @@
 #include <c_types.h>
 
 #include "stringutil.h"
+#include <osapi.h>
+#include <mem.h>
 
 char* ICACHE_FLASH_ATTR
 strnchr(char *pdata, char chr, unsigned short len) {
@@ -12,4 +14,43 @@ strnchr(char *pdata, char chr, unsigned short len) {
     i++;
   }
   return NULL;
+}
+
+bool ICACHE_FLASH_ATTR
+scan_token(scan_context_t* s_ctx,
+           const char* const pch_data,
+           uint16_t* const ui_len) {
+  uint16_t i = 0;
+  const size_t toklen = os_strlen(s_ctx->pch_token);
+  while (*ui_len != 0) {
+    (*ui_len)--;
+    if (pch_data[i++] == s_ctx->pch_token[s_ctx->ui_position++]) {
+      if (toklen == s_ctx->ui_position) {
+        return true;
+      }
+    } else {
+      s_ctx->ui_position = 0;
+    }
+  }
+  return false;
+}
+
+bool ICACHE_FLASH_ATTR
+scan_until(scan_context_t* s_ctx,
+           const char c,
+           const char* const pch_data,
+           uint16_t* const ui_len) {
+  uint16_t i = 0;
+  while (*ui_len != 0) {
+    (*ui_len)--;
+    // increase by one byte
+    s_ctx->pch_token = os_realloc(s_ctx->pch_token, s_ctx->ui_position + 1);
+    if (pch_data[i] == c) {
+      s_ctx->pch_token[s_ctx->ui_position] = '\0';
+      return true;
+    } else {
+      s_ctx->pch_token[s_ctx->ui_position++] = pch_data[i++];
+    }
+  }
+  return false;
 }
--- display/stringutil.h
+++ display/stringutil.h
@@ -1,6 +1,41 @@
 #ifndef _STRINGUTIL_H
 #define _STRINGUTIL_H
 
+#include <stdint.h>
+#include <stdbool.h>
+
 char* strnchr(char *pdata, char chr, unsigned short len);
+
+typedef struct {
+  char* pch_token;
+  uint32_t ui_position;
+} scan_context_t;
+
+/*
+ * scan contiguious datapackets for a given token, set ctx->pch_token
+ * to a zero terminated string of the token to search for and
+ * ctx->ui_position to zero.
+ *
+ * return true when found, and false if counsumed all input. ui_len is
+ * updated accordingly to how much data remains to be consumed.
+ */
+bool scan_token(scan_context_t* s_ctx,
+                const char* const pch_data,
+                uint16_t* const ui_len);
+
+
+/*
+ * scan contiguious datapackets and take bytes until an end char is
+ * found. ctx should be set to a zero'd structure before calling.
+ *
+ * return true when found, and false if counsumed all input. ui_len is
+ * updated accordingly to how much data remains to be
+ * consumed. ctx->pch_token will be set to a newly allocated zero
+ * terminated string.
+ */
+bool scan_until(scan_context_t* s_ctx,
+                const char c,
+                const char* const pch_data,
+                uint16_t* const ui_len);
 
 #endif // _STRINGUTIL_H
